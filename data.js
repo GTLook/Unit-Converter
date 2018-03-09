c1b03083199eaa7b78c39dd71b23e15fcd276d50
Gavin Look <GTLook@gmail.com>
2018-03-09 15:53:42 -0800

    basice HTML, Dataset, and JS created

---
+++ data.js
@@ -0,0 +1,54 @@
+const data = {
+
+'time':{
+  'second':	1,
+  'minute':	60,
+  'hour':	3600,
+  'day': 86400,
+  'week':	7*time.day
+},
+
+'distance':{
+  'meter':		1,
+  'feet':		0.3048,
+  'inch':		0.0254,
+  'yard':		0.9144,
+  'mile':		1609.34
+},
+
+'mass':{
+  'gram':		1,
+  'ton':			1000000,
+  'pound':		453.59237,
+  'ounce':		28.3495,
+  'stone':		6350.29
+},
+
+'volume':{
+  'teaspoon':		1,
+  'tablespoon':	3,
+  'ounce':		6,
+  'gill':		24,
+  'cup':			48,
+  'pint':			96,
+  'quart':		192,
+  'liter':		202.884,
+  'gallon':		768,
+  'barrel':		24192
+}
+
+  // storage:
+  //   bit,b			1bit
+  //   byte,B		8bit
+  //   kilobit,kb		1024bit
+  //   kilobyte,KB		1024byte
+  //   megabit,mb		1048576bit
+  //   megabyte,MB		1048576byte
+  //   gigabit,gb		1073741824bit
+  //   gigabyte,GB		1073741824byte
+  //   terabit,tb		1099511627776bit
+  //   terabyte,TB		1099511627776byte
+  //   petabit,pb		1125899906842624bit
+  //   petabyte,PB		1125899906842624byte
+
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+//link to data page
+let data = require('./data.js');
+
+const convert = document.querySelector('#convertButton');
+convert.addEventListener('click',function(){
+  console.log('convert!')
+})
